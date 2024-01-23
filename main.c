d3b51ea1416d229628ee444f1e09ae485e4f5c12
Beatriz Dile <bedos-sa@student.42sp.org.br>
2024-01-23 18:43:26 -0300

    ¨interface¨

--- main.c
+++ main.c
@@ -2,8 +2,17 @@
 
 int main()
 {
-	printf("CADETE PLANNER 42\nSELECIONE QUAL A SUA FASE: \n1 - ENTRANDO EM ORBITA\n2 - AMPLIANDO HORIZONTES\n3 - EXPLORANDO O ESPAÇO");
-	// scanf("");
+	printf("CADETE PLANNER 42\nSELECIONE QUAL A SUA FASE: \n1 - ENTRANDO EM ORBITA\n2 - AMPLIANDO HORIZONTES\n3 - EXPLORANDO O ESPAÇO\n");
+	int num;
+	scanf("%d", &num);
+	if (num == 1)
+		printf("1 - LIBFT\n2 - GET_NEXT_LINE\n3 - FT_PRINTF\n4 - BORN2BEROOT\n5 - SO_LONG\n6 - PIPEX\n");
+	if (num == 2)
+		printf("7 - PUSH_SWAP\n8 - MINISHELL\n9 - PHILOSOPHERS\n10 - NETPRACTICE\n11 - CUBE3D\n");
+	if (num == 3)
+		printf("12 - CPP\n13 - INCEPTION\n14 - WEBSERV\n15 - TRANCENDENCE\n");
+
+	scanf("%d", &num);
 
 }
 
