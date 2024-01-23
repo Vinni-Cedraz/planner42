9f3910f5789e66b629770edfad4359b1a9e2b881
Beatriz Dile <bedos-sa@student.42sp.org.br>
2024-01-23 17:41:31 -0300

    ¨main¨

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
-int main() {
-	printf("when the world is broken and the sky is cracked, come on to the house");
+#include <stdio.h>
+
+int main()
+{
+	printf("CADETE PLANNER 42\nSELECIONE QUAL A SUA FASE: \n1 - ENTRANDO EM ORBITA\n2 - AMPLIANDO HORIZONTES\n3 - EXPLORANDO O ESPAÇO");
+	// scanf("");
+
 }
+
--- teste.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("CADETE PLANNER 42\nSELECIONE QUAL A SUA FASE: \n1 - ENTRANDO EM ORBITA\n2 - AMPLIANDO HORIZONTES\n3 - EXPLORANDO O ESPAÇO");
-	// scanf("");
-
-}
-
